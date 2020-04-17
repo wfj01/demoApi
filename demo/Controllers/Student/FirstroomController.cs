c319c4d5ef140c6b59d21a89e776958af5956ffd
wfj <15263906353@163.com>
2020-04-17 15:25:37 +0800

    添加查询方法

--- demo/Controllers/Student/FirstroomController.cs
+++ demo/Controllers/Student/FirstroomController.cs
@@ -22,7 +22,7 @@ namespace demo.Api.Controllers
     public class FirstroomController : ControllerBase
     {
         /// <summary>
-        /// 第一餐厅-查询
+        /// 第一餐厅-加载表格数据
         /// </summary>
         /// <returns></returns>
         [HttpGet]
@@ -51,7 +51,161 @@ namespace demo.Api.Controllers
             {
                 return ApiResultBuilder<Firstroom>.Return(-2, "数据异常" + e.Message);
             }
+        }
 
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("SearchBtn")]
+        public JsonResult SearchBtn(string names, string question1, string question2)
+        {
+            if (names == null)
+            {
+                names = "";
+            }
+            if (question1 == null)
+            {
+                question1 = "";
+            }
+            if (question2 == null)
+            {
+                question2 = "";
+            }
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection("Server=localhost;User Id=sa;Password=123456789;Database=demo;");
+                sqlConnection.Open();
+                if (names == "" && question1 == "" && question2 == "")
+                {
+                    string sql = "SELECT * FROM [demo].[dbo].[firstroom]";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+                else if (names != "" && question1 == "" && question2 == "")
+                {
+                    string sql1 = "SELECT * FROM [demo].[dbo].[firstroom] where names like'" + names + "%'";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql1, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+                else if (names == "" && question1 != "" && question2 != "")
+                {
+                    string sql2 = "SELECT * FROM [demo].[dbo].[firstroom] WHERE question between'" + question1 + "'and'" + question2 + "'";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql2, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+                else if (names != "" && question1 != "" && question2 == "")
+                {
+
+                    string sql3 = "SELECT * FROM [demo].[dbo].[firstroom]  where names like'" + names + "%' and question ='" + question1 + "'";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql3, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+                else if (names != "" && question1 == "" && question2 != "")
+                {
+
+                    string sql4 = "SELECT * FROM [demo].[dbo].[firstroom]  where names like'" + names + "%' and question ='" + question2 + "'";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql4, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+                else if (names == "" && question1 == "" && question2 != "")
+                {
+                    string sql5 = "SELECT * FROM [demo].[dbo].[firstroom]  where  question ='" + question2 + "'";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql5, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+                else if (names == "" && question1 != "" && question2 == "")
+                {
+                    string sql6 = "SELECT * FROM [demo].[dbo].[firstroom]  where  question ='" + question1 + "'";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql6, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+                else
+                {
+
+                    string sql7 = "SELECT * FROM [demo].[dbo].[firstroom] where names like'" + names + "%' and question between '" + question1 + "'and'" + question2 + "'";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql7, sqlConnection);
+                    DataSet dataSet = new DataSet();
+                    sqlDataAdapter.Fill(dataSet);
+                    if (dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        return ApiResultBuilder<List<User>>.Return(dataSet);
+                    }
+                    else
+                    {
+                        return new JsonResult("查无数据");
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                return ApiResultBuilder<User>.Return(-2, "数据异常" + e.Message);
+            }
         }
     }
 }
